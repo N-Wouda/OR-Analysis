988edf36d19de348380daf503af9ea9e2e265f08
Niels Wouda <nielswouda@gmail.com>
2020-02-17 14:12:58 +0100

    Auto stash before merge of "random-greedy" and "origin/random-greedy"

--- heuristic/functions/customers_to_remove.py
+++ heuristic/functions/customers_to_remove.py
@@ -2,5 +2,5 @@ from ..constants import DEGREE_OF_DESTRUCTION
 from ..classes import Problem
 
 
-def customers_to_remove(problem: Problem):
+def customers_to_remove(problem: Problem) -> int:
     return int(problem.num_customers * DEGREE_OF_DESTRUCTION)
