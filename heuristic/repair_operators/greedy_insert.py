e8414236f73ce93f00cc1ed2b7df7b3bab8437ed
Niels Wouda <nielswouda@gmail.com>
2020-02-18 11:34:44 +0100

    Some clean-up for greedy_insert

--- heuristic/repair_operators/greedy_insert.py
+++ heuristic/repair_operators/greedy_insert.py
@@ -11,8 +11,8 @@ def greedy_insert(current: Solution, rnd_state: RandomState) -> Solution:
     """
     while len(current.unassigned) != 0:
         customer = current.unassigned.pop()
-
         insertion_costs = []
+
         # Costs inserting in an existing route
         for route in current.routes:
             full_route = np.array([DEPOT, *route.customers, DEPOT])
@@ -20,10 +20,10 @@ def greedy_insert(current: Solution, rnd_state: RandomState) -> Solution:
 
             insertion_costs.append([current.problem.distances
                                     [first, customer + 1]
-                                   + current.problem.distances
+                                    + current.problem.distances
                                     [customer + 1, second]
-                                   for first, second in
-                                   zip(full_route, full_route[1:])])
+                                    for first, second in
+                                    zip(full_route, full_route[1:])])
 
         # Costs new route
         insertion_costs.append([current.problem.distances
@@ -40,16 +40,13 @@ def greedy_insert(current: Solution, rnd_state: RandomState) -> Solution:
                    for idx, route_insertion_costs in enumerate(insertion_costs)
                    if min_cost in insertion_costs[idx]]
 
-        route_index = indexes[0][0]
-        customer_index = indexes[0][1]
+        idx_route, idx_customer = indexes[0]
 
-        delivery = Item(current.problem.demands[customer], DEPOT,
-                        customer)
-        pickup = Item(current.problem.pickups[customer], customer,
-                      DEPOT)
+        delivery = Item(current.problem.demands[customer], DEPOT, customer)
+        pickup = Item(current.problem.pickups[customer], customer, DEPOT)
 
         # If creating a new route is cheapest
-        if route_index == len(current.routes):
+        if idx_route == len(current.routes):
             stacks = [Stacks(current.problem.num_stacks) for _ in range(2)]
 
             stacks[0].shortest_stack().push_rear(delivery)
@@ -57,23 +54,23 @@ def greedy_insert(current: Solution, rnd_state: RandomState) -> Solution:
 
             current.routes.append(Route([customer], stacks))
         else:
-            route = current.routes[route_index]
+            route = current.routes[idx_route]
 
             # insert in cheapest route
-            route.customers.insert(customer_index, customer)
-            route._set.add(customer)
+            route.customers.insert(idx_customer, customer)
+            route._set.add(customer)  # TODO this is not very nice
 
             # insert handling plan at added customer
-            stack_after_customer = Stacks.copy(route.plan[customer_index])
+            stack_after_customer = Stacks.copy(route.plan[idx_customer])
 
-            route.plan.insert(customer_index + 1, stack_after_customer)
+            route.plan.insert(idx_customer + 1, stack_after_customer)
 
             # insert for delivery in all stacks before customer
-            for idx in range(customer_index + 1):
-                route.plan[idx].shortest_stack().push_rear(delivery)
+            for plan in route.plan[:idx_customer + 1]:
+                plan.shortest_stack().push_rear(delivery)
 
             # insert pickup for all stacks after customer
-            for idx in range(customer_index + 1, len(route.plan)):
-                route.plan[idx].shortest_stack().push_rear(pickup)
+            for plan in route.plan[idx_customer + 1:]:
+                plan.shortest_stack().push_rear(pickup)
 
     return current
