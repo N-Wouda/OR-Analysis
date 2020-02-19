83f5c6c85cfc1c32032130786464f8198192d69b
Niels Wouda <nielswouda@gmail.com>
2020-02-19 23:25:00 +0100

    More explanations

--- heuristic/repair_operators/random_repair.py
+++ heuristic/repair_operators/random_repair.py
@@ -16,17 +16,18 @@ def random_repair(current: Solution, rnd_state: RandomState) -> Solution:
     while len(current.unassigned) != 0:
         customer = current.unassigned.pop()
 
-        route_indices = rnd_state.permutation(len(current.routes))
-
-        for idx_route in route_indices:
+        # Traverses a random permutation of the route indices - this ensures
+        # we insert into a random route, if that's feasible.
+        for idx_route in rnd_state.permutation(len(current.routes)):
             route = current.routes[idx_route]
-
             insert_idx = rnd_state.randint(len(route.customers))
 
             if route.can_insert(customer, insert_idx, current.problem):
                 route.insert_customer(customer, insert_idx, current.problem)
                 break
 
+        # There is no feasible route to insert into, so we create a new route
+        # for just this customer.
         if not any(customer in route for route in current.routes):
             route = create_single_customer_route(customer, current.problem)
             current.routes.append(route)
