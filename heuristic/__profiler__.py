aad60320982147ec65077305470d365d1366e223
Niels Wouda <nielswouda@gmail.com>
2020-02-19 12:22:13 +0100

    Profiler option

---
+++ heuristic/__profiler__.py
@@ -0,0 +1,5 @@
+# Executes the module from a script, which is what cProfile expects. Based on
+# https://intellij-support.jetbrains.com/hc/en-us/community/posts/115000023670
+from heuristic.__main__ import main
+
+main()
