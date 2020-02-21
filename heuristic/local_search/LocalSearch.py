e27453f1c1549a91c1bf5960f01fe8cecd871ec7
Niels Wouda <nielswouda@gmail.com>
2020-02-21 17:48:55 +0100

    Local search algorithm from Hornstra paper

--- heuristic/local_search/LocalSearch.py
+++ heuristic/local_search/LocalSearch.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, List
 
 from numpy.random import RandomState
 
@@ -8,10 +8,20 @@ from heuristic.classes import Solution
 class LocalSearch:
 
     def __init__(self):
-        self.operators = []
+        self.operators: List[Callable[..., Solution]] = []
 
     def add_operator(self, operator: Callable):
         self.operators.append(operator)
 
     def __call__(self, current: Solution, rnd_state: RandomState) -> Solution:
-        return current  # TODO
+        def improve(solution):
+            for operator in self.operators:
+                new_solution = operator(solution, rnd_state)
+
+                if new_solution.objective() < solution.objective():
+                    solution = improve(new_solution)
+                    break
+
+            return solution
+
+        return improve(current.copy())
