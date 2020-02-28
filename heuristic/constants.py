5addb7b6ce5dcb19f2c78793f1d39e3972e3cfd6
Niels Wouda <nielswouda@gmail.com>
2020-02-28 10:36:49 +0100

    Update ALNS, RRT criterion

--- heuristic/constants.py
+++ heuristic/constants.py
@@ -1,9 +1,9 @@
-from alns.criteria import HillClimbing
+from alns.criteria import RecordToRecordTravel
 
 DEPOT = -1
 TEAM_NUMBER = 3
 DEGREE_OF_DESTRUCTION = 0.25
 WEIGHTS = [25, 5, 1, 1]
 DECAY = 0.8
-CRITERION = HillClimbing()
-ITERATIONS = 100
+ITERATIONS = 25000
+CRITERION = RecordToRecordTravel(200, 1, step=199 / ITERATIONS)
