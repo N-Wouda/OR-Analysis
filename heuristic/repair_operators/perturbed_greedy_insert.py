a9317f6c8fad970c4df4f42ff7b4f906bd30b356
Emiel <Emielkrol@gmail.com>
2020-03-03 17:58:49 +0100

    Add _yth_best_insert

--- heuristic/repair_operators/perturbed_greedy_insert.py
+++ heuristic/repair_operators/perturbed_greedy_insert.py
@@ -1,8 +1,8 @@
-import heapq
 from numpy.random import RandomState
 
-from heuristic.classes import Route, Solution
-from heuristic.functions import create_single_customer_route
+from heuristic.classes import Solution
+from heuristic.constants import MIN_OFFSET
+from ._yth_best_insert import _yth_best_insert
 
 
 def perturbed_greedy_insert(current: Solution,
@@ -13,31 +13,5 @@ def perturbed_greedy_insert(current: Solution,
 
     Perturbed sequential best insertion in Hornstra et al. (2020).
     """
-    rnd_state.shuffle(current.unassigned)
 
-    while len(current.unassigned) != 0:
-        customer = current.unassigned.pop()
-        feasible_routes = []
-
-        for route in current.routes:
-            insert_idx, cost = route.opt_insert(customer)
-
-            if route.can_insert(customer, insert_idx):
-                heapq.heappush(feasible_routes, (cost, insert_idx, route))
-
-        if len(feasible_routes) != 0:
-            y = min(3, len(feasible_routes))
-            nsmallest = heapq.nsmallest(y, feasible_routes)
-            rnd_state.shuffle(nsmallest)
-
-            cost, insert_idx, route = nsmallest[0]
-            cost_new = Route([customer], []).routing_cost()
-
-            if cost_new > cost:
-                route.insert_customer(customer, insert_idx)
-                continue
-
-        route = create_single_customer_route(customer)
-        current.routes.append(route)
-
-    return current
+    return _yth_best_insert(MIN_OFFSET, current, rnd_state)
