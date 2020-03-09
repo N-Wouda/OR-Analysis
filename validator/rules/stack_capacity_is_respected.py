0919e7829a74d5ad70fd88d22dcaf6e73a89c7c5
Niels Wouda <nielswouda@gmail.com>
2020-03-09 11:59:07 +0100

    Simplify stack capacity validator rule

--- validator/rules/stack_capacity_is_respected.py
+++ validator/rules/stack_capacity_is_respected.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-from heuristic.classes import Problem, Solution
+from heuristic.classes import Solution
 
 
 def stack_capacity_is_respected(solution: Solution) -> Tuple[bool, str]:
@@ -8,12 +8,8 @@ def stack_capacity_is_respected(solution: Solution) -> Tuple[bool, str]:
     Verifies the stack capacities are respected for each leg in each tour of
     the solution.
     """
-    problem = Problem()
-
     for route in solution.routes:
-        for stacks in route.plan:
-            if any(stack.volume() > problem.stack_capacity
-                   for stack in stacks):
-                return False, "Stack capacity is not respected."
+        if not all(stacks.is_feasible() for stacks in route.plan):
+            return False, "Stack capacity is not respected."
 
     return True, "Stack capacity is respected."
