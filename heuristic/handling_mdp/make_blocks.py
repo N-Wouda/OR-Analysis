57560ae1f5594a1e072119889921265a888d5969
Niels Wouda <nielswouda@gmail.com>
2020-03-09 18:05:39 +0100

    Simplify make_blocks

--- heuristic/handling_mdp/make_blocks.py
+++ heuristic/handling_mdp/make_blocks.py
@@ -11,48 +11,16 @@ def make_blocks(route: Route) -> List[Block]:
     """
     Constructs suitable blocks for the given route. These adhere to the single
     invariant that each block can be placed in each position, without violating
-    stack capacity constraints (they are perfectly interchangeable).
+    stack capacity constraints (they are nearly completely interchangeable).
     """
-    problem = Problem()
-
     if len(route.customers) <= NUM_BLOCKS:
         # This we can solve optimally, with a block for each customer.
         blocks = [Block([]) for _ in range(NUM_BLOCKS - len(route.customers))]
         blocks.extend(Block([customer]) for customer in route.customers)
-
-        assert len(blocks) == NUM_BLOCKS
-        return blocks
-
-    blocks = []
-
-    # Create NUM_BLOCKS / num_stacks blocks for each stack by balancing the
-    # customer item sizes between blocks.
-    for idx, stack in _customers_by_stack(route):
-        for customers in split(stack, NUM_BLOCKS // problem.num_stacks):
-            blocks.append(Block(customers))
-
-    while len(blocks) != NUM_BLOCKS:
-        largest = max(blocks, key=lambda block: block.max_capacity_used())
-
-        blocks.remove(largest)
-        blocks.extend(largest.split())
+    else:
+        # Create NUM_BLOCKS of customers by grouping nearby customers on the
+        # route. These blocks are approximately balanced.
+        blocks = list(map(Block, split(route.customers.to_list(), NUM_BLOCKS)))
 
     assert len(blocks) == NUM_BLOCKS
     return blocks
-
-
-def _customers_by_stack(route: Route):
-    problem = Problem()
-
-    by_stack = defaultdict(list)
-
-    for customer in route.customers:
-        # Find the stack the customer's items are assigned to at the depot. We
-        # will use this to determine the block assignments.
-        stack = route.plan[0].find_stack(problem.demands[customer])
-        by_stack[stack.index].append(customer)
-
-    # There's no point in returning an empty stack - we might just as well
-    # skip it here.
-    return [(idx, customers) for idx, customers in by_stack.items()
-            if len(customers) != 0]
