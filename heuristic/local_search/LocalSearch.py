8ed9d95d2d0cf4dd9ba234cce8dc4a857571de6e
Niels Wouda <nielswouda@gmail.com>
2020-03-10 16:39:00 +0100

    Intra 2-opt

--- heuristic/local_search/LocalSearch.py
+++ heuristic/local_search/LocalSearch.py
@@ -16,27 +16,41 @@ class LocalSearch:
         self.operators.append(operator)
 
     def __call__(self, current: Solution, rnd_state: RandomState) -> Solution:
-        improved = current.copy()
 
-        for route in improved.routes:
-            if np.isclose(route.handling_cost(), 0.):
-                continue  # we cannot improve upon this.
+        def improve(solution):
+            for operator in self.operators:
+                new_solution = operator(solution, rnd_state)
 
-            plan = MDP.from_route(route).solve()
+                if new_solution.objective() < solution.objective():
+                    return improve(new_solution)
 
-            before = route.handling_cost()
-            after = Route(route.customers, plan).handling_cost()
+            return solution
 
-            if before < after:
-                # Even with an MDP, this can happen as we implicitly assume
-                # an equal number of blocks are assigned to each stack. That
-                # is not always optimal, hence this check.
-                # TODO extend the state space to allow the stack assignment to
-                #  be done explicitly by the algorithm?
-                continue
+        improved = improve(current.copy())
 
-            route.plan = plan
-            route.invalidate_handling_cache()
+        for route in improved.routes:  # TODO when should we do this?
+            self._apply_mdp(route)
 
         assert improved.objective() <= current.objective()
         return improved
+
+    @staticmethod
+    def _apply_mdp(route: Route):
+        if np.isclose(route.handling_cost(), 0.):
+            return  # we cannot improve upon this.
+
+        plan = MDP.from_route(route).solve()
+
+        before = route.handling_cost()
+        after = Route(route.customers, plan).handling_cost()
+
+        if before < after:
+            # Even with an MDP, this can happen as we implicitly assume
+            # an equal number of blocks are assigned to each stack. That
+            # is not always optimal, hence this check.
+            # TODO extend the state space to allow the stack assignment to
+            #  be done explicitly by the algorithm?
+            return
+
+        route.plan = plan
+        route.invalidate_handling_cache()
