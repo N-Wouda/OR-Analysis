7294484d579e6815a2572fae7c5358cc447beed4
Emiel <Emielkrol@gmail.com>
2020-03-16 22:36:19 +0100

    Add the constraints to ILP

--- ilp/constraints/demand.py
+++ ilp/constraints/demand.py
@@ -8,8 +8,9 @@ def deliveries(problem: Problem, solver):
     """
     for customer_1 in range(1, problem.num_customers):
         assignments = solver.sum(
+            solver.delivery_volumes[customer_2, customer_1, stack, index] -
             solver.delivery_volumes[customer_1, customer_2, stack, index]
-            for customer_2 in range(problem.num_customers)
+            for customer_2 in range(problem.num_customers + 1)
             for stack in range(problem.num_stacks)
             for index in range(MAX_STACK_INDEX))
         solver.add_constraint(assignments == problem.demands[customer_1])
---
+++ ilp/constraints/pickup_moved_2.py
@@ -0,0 +1,27 @@
+from heuristic.classes import Problem
+from heuristic.constants import M, MAX_STACK_INDEX
+
+
+def demand_moved_2(problem: Problem, solver):
+    """
+    Ensures the moved variable is 1 if the delivery volume is higher after
+    visiting a customer for every index in every stack.
+    """
+    for customer_1 in range(1, problem.num_customers):
+        for stack in range(problem.num_stacks):
+            for index in range(MAX_STACK_INDEX):
+                before = solver.sum(
+                    solver.delivery_volumes[customer_1,
+                                            customer_2,
+                                            stack,
+                                            index]
+                    for customer_2 in range(problem.num_customers))
+
+                after = solver.sum(
+                    solver.delivery_volumes[customer_2,
+                                            customer_1,
+                                            stack,
+                                            index]
+                    for customer_2 in range(problem.num_customers))
+
+                solver.add_constraint(after - before <= M * solver.is_moved)
