b633da1576083abc4f53cfd6d94879df79cfaaff
Niels Wouda <nielswouda@gmail.com>
2020-03-24 18:56:53 +0100

    Remove missing operator from __init__

--- heuristic/local_search/__init__.py
+++ heuristic/local_search/__init__.py
@@ -3,7 +3,6 @@ from typing import Callable, List
 from heuristic.classes import Route
 from .LocalSearch import LocalSearch
 from .item_reinsert import item_reinsert
-from .in_stack_roll_delivery_to_rear import in_stack_roll_delivery_to_rear
 from .pickup_push_to_front import pickup_push_to_front
 from .in_route_two_opt import in_route_two_opt
 
@@ -11,6 +10,5 @@ L_OPERATORS: List[Callable[[Route], Route]] = [
     in_route_two_opt,
     # TODO more routing options
     item_reinsert,
-    in_stack_roll_delivery_to_rear,
     pickup_push_to_front,
 ]
