d6fc59cb67308d2dbe4dc5180e5e94c2d16d15cb
Niels Wouda <nielswouda@gmail.com>
2020-03-25 21:05:04 +0100

    TODO in relocate to fix Travis

--- heuristic/local_search/relocate_customer.py
+++ heuristic/local_search/relocate_customer.py
@@ -38,12 +38,16 @@ def relocate_customer(solution: Solution) -> Solution:
                 if gain < 0:
                     insert_locations.push(gain, (customer, idx, route))
 
-    if len(insert_locations) != 0:
+    while len(insert_locations) != 0:
         # We do not check for the handling effects here - although that is of
         # course a serious consideration, getting customers into the proper
         # routes counts a lot more at the solution level. The route based
         # operators will likely repair any inefficiencies due to handling.
         _, (customer, insert_idx, next_route) = insert_locations.pop()
+
+        # TODO this does not work for small instances, because some routes
+        #  turn empty once the customer is removed (insertion then has an
+        #  index that's too large). Think about how to fix.
 
         route = solution.find_route(customer)
         route.remove_customer(customer)
