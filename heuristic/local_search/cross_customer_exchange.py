54375b5f9b5826c0f903a6663f101bf915a52fcc
Niels Wouda <nielswouda@gmail.com>
2020-03-26 17:21:16 +0100

    Several to-do's for cross exchange

--- heuristic/local_search/cross_customer_exchange.py
+++ heuristic/local_search/cross_customer_exchange.py
@@ -5,6 +5,15 @@ from heuristic.functions import remove_empty_routes, routing_costs
 
 def cross_customer_exchange(solution: Solution) -> Solution:
     """
-    TODO.
+    Tries to remove crossing links between routes.
+    TODO neighbourhood size
+    TODO best move? First better?
+
+    References
+    ----------
+    - Savelsbergh, Martin W. P. 1992. "The Vehicle Routing Problem with Time
+      Windows: Minimizing Route Duration." *ORSA Journal on Computing* 4 (2):
+      146-154.
     """
+
     return solution
