571211598273cdd9a664a2cfe1c0097d3fec58bb
Niels Wouda <nielswouda@gmail.com>
2020-03-28 10:17:32 +0100

    Exchange customer operator

--- heuristic/local_search/__init__.py
+++ heuristic/local_search/__init__.py
@@ -3,6 +3,7 @@ from typing import Callable, List
 from heuristic.classes import Route, Solution
 from .LocalSearch import LocalSearch
 from .cross_customer_exchange import cross_customer_exchange
+from .exchange_customer import exchange_customer
 from .in_route_two_opt import in_route_two_opt
 from .item_reinsert import item_reinsert
 from .pickup_push_to_front import pickup_push_to_front
@@ -11,6 +12,7 @@ from .relocate_customer import relocate_customer
 SOLUTION_OPERATORS: List[Callable[[Solution], Solution]] = [
     cross_customer_exchange,
     relocate_customer,
+    exchange_customer,
     # TODO more routing options
 ]
 
--- heuristic/local_search/exchange_customer.py
+++ heuristic/local_search/exchange_customer.py
@@ -1,15 +1,13 @@
 from copy import deepcopy
 from itertools import product
 
-from heuristic.classes import Heap, Problem, Solution
+from heuristic.classes import Heap, Problem, Solution, Route
 from heuristic.constants import DEPOT
 
 
-def cross_customer_exchange(solution: Solution) -> Solution:
+def exchange_customer(solution: Solution) -> Solution:
     """
-    Tries to remove crossing links between routes.
-    TODO neighbourhood size
-    TODO best move? First better?
+    TODO
 
     References
     ----------
@@ -18,33 +16,31 @@ def cross_customer_exchange(solution: Solution) -> Solution:
       146-154.
     """
     feasible_moves = Heap()
-
+    print()
+    print(solution.objective())
     for idx1, route1 in enumerate(solution.routes):
         for idx2, route2 in enumerate(solution.routes[idx1 + 1:], idx1 + 1):
             for first, second in product(enumerate(route1), enumerate(route2)):
                 if _gain(route1, *first, route2, *second) >= 0:
                     continue
 
-                c_idx1, _ = first
-                c_idx2, _ = second
-
-                first_customers = route1.customers[c_idx1 + 1:]
-                second_customers = route2.customers[c_idx2 + 1:]
+                c_idx1, customer1 = first
+                c_idx2, customer2 = second
 
                 new_route1 = deepcopy(route1)
                 new_route2 = deepcopy(route2)
 
-                for customer in first_customers:
-                    new_route1.remove_customer(customer)
+                new_route1.remove_customer(customer1)
+                new_route2.remove_customer(customer2)
 
-                for customer in second_customers:
-                    new_route2.remove_customer(customer)
-
-                if not new_route1.attempt_append_tail(second_customers):
+                if not new_route1.can_insert(customer2, c_idx1):
                     continue
 
-                if not new_route2.attempt_append_tail(first_customers):
+                if not new_route2.can_insert(customer1, c_idx2):
                     continue
+
+                new_route1.insert_customer(customer2, c_idx1)
+                new_route2.insert_customer(customer1, c_idx2)
 
                 new = new_route1.cost() + new_route2.cost()
                 old = route1.cost() + route2.cost()
@@ -59,19 +55,23 @@ def cross_customer_exchange(solution: Solution) -> Solution:
         solution.routes[idx1] = new_route1
         solution.routes[idx2] = new_route2
 
+    print(solution.objective())
     return solution
 
 
 def _gain(route1, idx1, customer1, route2, idx2, customer2):
+    prev1 = DEPOT if idx1 == 0 else route1.customers[idx1 - 1]
     next1 = DEPOT if idx1 == len(route1) - 1 else route1.customers[idx1 + 1]
+
+    prev2 = DEPOT if idx1 == 0 else route2.customers[idx2 - 1]
     next2 = DEPOT if idx2 == len(route2) - 1 else route2.customers[idx2 + 1]
 
     problem = Problem()
 
-    gain = problem.distances[customer2 + 1, next1 + 1]
-    gain += problem.distances[customer1 + 1, next2 + 1]
+    gain = problem.short_distances[prev1 + 1, customer2 + 1, next1 + 1]
+    gain += problem.short_distances[prev2 + 1, customer1 + 1, next2 + 1]
 
-    gain -= problem.distances[customer1 + 1, next1 + 1]
-    gain -= problem.distances[customer2 + 1, next2 + 1]
+    gain -= problem.short_distances[prev1 + 1, customer1 + 1, next1 + 1]
+    gain -= problem.short_distances[prev2 + 1, customer2 + 1, next2 + 1]
 
     return gain
